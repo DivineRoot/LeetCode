cc8f7663f0a1282ec89652aa727f66a50d34cbb7
Debakalpa Roy <debakalparoy@gmail.com>
2022-03-16 00:24:16 +0530

    Time: 11 ms (84.30%), Space: 18.5 MB (37.88%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        int i = 0, j = 1, count = 1;
+        if(nums.size() == 0) return 0;
         
-        if(nums.size() == 0)
-            return 0;
-        
-        for(j = 1; j < nums.size(); j++){
-            if(nums[j] != nums[i] ){
-                i++;
-                nums[i] = nums[j];
-                count++;
-            }
-        }
-        return count;           // OR return i+1 Since, i would be on the desired once j goes out of bound.
+        int left = 0;
+        for(int right =1; right< nums.size(); right++){
+           if(nums[left] != nums[right])
+               left++;
+               nums[left] = nums[right];
+           }
+    return left+1;
     }
 };
