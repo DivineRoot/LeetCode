b825d7b94f5e8b647158883b610f7e09f745993c
Debakalpa Roy <debakalparoy@gmail.com>
2022-03-18 03:12:54 +0530

    Time: 3 ms (94.04%), Space: 10.5 MB (87.16%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,26 +1,29 @@
 class Solution {
 public:
-    void findCombinationSum2(int ind, int target, vector<int> &arr, vector<vector<int>> &ans, vector<int> &ds){
-       if(target == 0){
+    void helper(int index, int target, vector<int>&candidates, vector<vector<int>>&ans, vector<int>&ds){
+        //base case
+        //when the combination ds is carrying is a valid one add it into the ans
+        if(target == 0){
             ans.push_back(ds);
             return;
         }
-        
-        for(int i=ind; i< arr.size(); i++){
-            if(i > ind && arr[i] == arr[i-1])
+        for(int i = index; i< candidates.size(); i++){
+        // no point of it to add it to the ans if the  candidate at ith element exceeds the target no picking up hence break
+            if(candidates[i] > target) break;  
+            if(i  > index && candidates[i] == candidates[i-1])
                 continue;
-            if(arr[ind] > target)   break;
-            ds.push_back(arr[i]);
-            findCombinationSum2(i+1, target-arr[i], arr, ans, ds);
+            //to avoid picking up the same elements and thus avoiding duplicates (no picking of element)
+            ds.push_back(candidates[i]);
+            helper(i+1, target- candidates[i], candidates, ans, ds); //call for next  index element
             ds.pop_back();
         }
+        
     }
-    
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
-        vector<vector<int>> ans;
-        vector<int> ds;
-        sort(candidates.begin(), candidates.end());
-        findCombinationSum2(0, target, candidates, ans, ds);
+        sort(candidates.begin(), candidates.end());//sort the given candidates vector 
+        vector<vector<int>>ans;
+        vector<int>ds;
+        helper(0, target, candidates, ans, ds);
         return ans;
     }
 };
