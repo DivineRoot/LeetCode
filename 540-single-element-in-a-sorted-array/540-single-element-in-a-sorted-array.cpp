f10707b13e80426ee782de0c6796b9fd2c8774f4
Debakalpa Roy <debakalparoy@gmail.com>
2022-03-24 23:57:20 +0530

    Time: 34 ms (59.20%), Space: 22.4 MB (56.05%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.cpp
@@ -1,4 +1,22 @@
-// Bit maniuplation:    TC: O(N)        SC: O(1) 
+//Binary Search         TC: O(logN)         SC: O(1)
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int l=0, r=nums.size()-1, mid, num;
+        
+        while(l < r){
+            mid = l+(r-l)/2;
+            num = (mid%2 == 0) ? mid+1 : mid-1;
+            if(nums[mid]==nums[num]) l = mid+1;
+            else r = mid;
+        }
+        return nums[l];
+    }
+};
+
+/*
+// Bit maniuplation:    TC: O(N)            SC: O(1) 
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
@@ -9,3 +27,4 @@ public:
        return element; 
     }
 };
+*/
