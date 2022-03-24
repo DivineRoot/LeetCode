d112f736f022d377ce048e0b048d322ce12a272e
Debakalpa Roy <debakalparoy@gmail.com>
2022-03-25 02:22:53 +0530

    Time: 10 ms (19.54%), Space: 11.1 MB (75.66%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.cpp
@@ -9,10 +9,11 @@ public:
             // If the left half is sorted
             if(nums[low] <= nums[mid]){
                 if( nums[low] <= target  && target <= nums[mid])
-                    high = mid - 1;             // if the target lie in the sorted left half
+                    high = mid - 1;             // if the target lies in the sorted left half
                 else
                     low = mid + 1;              // if the target doesn't lie in the sorted left half
             }
+            // If the right half is sorted
             else{
                 if(nums[mid] <= target && target <= nums[high])
                     low = mid + 1;
