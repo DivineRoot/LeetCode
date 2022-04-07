a4bf2d87afd945ada40c0e43660599382a78ff07
Debakalpa Roy <debakalparoy@gmail.com>
2022-04-07 23:29:27 +0530

    Time: 7 ms (12.87%), Space: 7.6 MB (34.27%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
@@ -10,5 +11,19 @@ public:
             if(x != y)  pq.push(y - x);
         }
         return pq.empty()? 0 : pq.top(); 
+    }
+};
+*/
+
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        sort(stones.begin(), stones.end());
+        for(int i = n-1; i>0; i--){
+            stones[i-1] = stones[i] - stones[i-1];
+            sort(stones.begin(), stones.end());
+        }
+        return stones[0];
     }
 };
