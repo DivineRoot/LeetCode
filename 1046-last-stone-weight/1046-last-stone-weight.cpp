eb70ed568c0d3125621528b48f821db6f28d9547
Debakalpa Roy <debakalparoy@gmail.com>
2022-04-07 23:38:09 +0530

    Time: 8 ms (6.72%), Space: 7.5 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,3 +1,22 @@
+/* 1. Using Priority Queue: TC: O(N)    SC:O(1) */
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue pq(stones.begin(), stones.end());
+        while(pq.size() > 1){
+            int y = pq.top();
+            pq.pop();
+            int x = pq.top();
+            pq.pop();
+            if(x != y)  pq.push(y - x);
+        }
+        return pq.empty()? 0 : pq.top(); 
+    }
+};
+
+/*
+2. Using Heap: TC: O(N)    SC:O(1)
+
 class Solution {
 public:
     int lastStoneWeight(vector<int>& s) {
@@ -21,3 +40,19 @@ public:
         return s.size() ? s.front() : 0;
     }
 };
+
+3. Using sorting & replacing end no's by their diff.
+    TC: O(NlogN) + (O(N) * O(NlogN)) ==> O(NlogN) + O(N^2logN)      SC: O(1)
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n = stones.size();
+        sort(stones.begin(), stones.end());
+        for(int i = n-1; i>0; i--){
+            stones[i-1] = stones[i] - stones[i-1];
+            sort(stones.begin(), stones.end());
+        }
+        return stones[0];
+    }
+};
+*/
