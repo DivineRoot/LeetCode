c850b26f5a606f58235f9f63a5741a245422d24d
Debakalpa Roy <debakalparoy@gmail.com>
2022-04-07 23:37:57 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (78.96%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.cpp
+++ 1046-last-stone-weight/1046-last-stone-weight.cpp
@@ -1,29 +1,23 @@
-/*
 class Solution {
 public:
-    int lastStoneWeight(vector<int>& stones) {
-        priority_queue pq(stones.begin(), stones.end());
-        while(pq.size() > 1){
-            int y = pq.top();
-            pq.pop();
-            int x = pq.top();
-            pq.pop();
-            if(x != y)  pq.push(y - x);
+    int lastStoneWeight(vector<int>& s) {
+         make_heap(s.begin(), s.end());
+        
+         while (s.size() > 1) {
+            int x = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            int y = s.front();
+            pop_heap(s.begin(), s.end());
+            s.pop_back();
+            
+            if (x == y) continue;
+            
+            s.push_back(x - y);
+            push_heap(s.begin(), s.end());
         }
-        return pq.empty()? 0 : pq.top(); 
-    }
-};
-*/
-
-class Solution {
-public:
-    int lastStoneWeight(vector<int>& stones) {
-        int n = stones.size();
-        sort(stones.begin(), stones.end());
-        for(int i = n-1; i>0; i--){
-            stones[i-1] = stones[i] - stones[i-1];
-            sort(stones.begin(), stones.end());
-        }
-        return stones[0];
+        
+        return s.size() ? s.front() : 0;
     }
 };
