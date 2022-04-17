d8107d76ea775afcb070115f396047d34238185b
Debakalpa Roy <debakalparoy@gmail.com>
2022-04-17 13:28:21 +0530

    Time: 9 ms (90.04%), Space: 10.9 MB (44.44%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,4 +1,17 @@
 class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> ans;
+        for(int i=0; i<nums.size(); i++){
+            if(ans.find(target - nums[i])!= ans.end())
+                return {ans[target - nums[i]], i};
+            ans[nums[i]] = i;
+        }
+        return { };
+    }
+};
+/* 
+class Solution {
 public:    
     // Brute Force: TC: O(N^2)  SC: O(N)
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -6,12 +19,14 @@ public:
         for(int i=0; i<nums.size(); i++){
             for(int j=i+1; j<nums.size(); j++){
                 if(nums[i] + nums[j] == target){
+                    return {i, j};
+                        OR
                     ans.push_back(i);
                     ans.push_back(j);
                     break;
                 }
             }
         }
-        return ans;
+        return { } OR return ans;
     }
-};
+};*/
